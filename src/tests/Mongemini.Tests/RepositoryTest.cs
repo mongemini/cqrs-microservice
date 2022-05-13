10699fa7e17214aacbae3522cd5dccf2560811b4
mongemini <mongemini@gmail.com>
2022-05-13 22:17:28 +0300

    add test for repository

---
+++ src/tests/Mongemini.Tests/RepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿using Mongemini.Service.Infrastructure.Entities;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace Mongemini.Tests
+{
+    public class RepositoryTest
+    {
+        [Fact]
+        public async void SaveAsync_Blank_Test()
+        {
+
+            var repository = TestHelper.GetRepository();
+
+            await repository.AddAsync(new BlankEntity() { Id = "id1", SomeText = "some test" }, CancellationToken.None);
+
+            await repository.SaveAsync(CancellationToken.None);
+
+            var results = repository.GetAll();
+            var blank = results.FirstOrDefault();
+
+            Assert.NotNull(results);
+            Assert.NotNull(blank);
+            Assert.Equal("some test", blank.SomeText);
+        }
+    }
+}
---
+++ src/tests/Mongemini.Tests/TestHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mongemini.Service.Infrastructure;
+using Mongemini.Service.Infrastructure.Contracts;
+using Mongemini.Service.Infrastructure.Repositories;
+
+namespace Mongemini.Tests
+{
+    public class TestHelper
+    {
+        public static IBlankContext GetContext()
+        {
+            var builder = new DbContextOptionsBuilder<BlankContext>();
+            builder.UseInMemoryDatabase(databaseName: "BaseDbContextInMemory");
+
+            var dbContextOptions = builder.Options;
+            var libraryDbContext = new BlankContext(dbContextOptions);
+            
+            libraryDbContext.Database.EnsureDeleted();
+            libraryDbContext.Database.EnsureCreated();
+
+            return libraryDbContext;
+        }
+
+        public static BlankRepository GetRepository()
+        {
+            return new BlankRepository(GetContext());
+        }
+    }
+}
